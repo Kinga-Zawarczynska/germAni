1b273348cd292993d93013cb589a9fe3843dfdce
Wojtek Szczawinski <szczawinski.wojtek@gmail.com>
2020-07-30 16:18:36 +0200

    feat(favicons): change favicon to white one when in dark mode, font cdn

--- src/app/app.js
+++ src/app/app.js
@@ -2,4 +2,22 @@ export const run = (stickyNavabar, asidePic) => {
     window.onscroll = () => {
         stickyNavabar.handleStickyNavbar(), asidePic.handleChangeAsidePic();
     };
+    window.addEventListener('DOMContentLoaded', setFavicon);
+
+    function setFavicon() {
+        if (
+            window.matchMedia &&
+            window.matchMedia('(prefers-color-scheme: dark)').matches
+        ) {
+            const favicon = document.querySelector('[data-dark]');
+
+            if (!favicon) {
+                return;
+            }
+
+            if (favicon.dataset.dark) {
+                favicon.href = favicon.dataset.dark;
+            }
+        }
+    }
 };
