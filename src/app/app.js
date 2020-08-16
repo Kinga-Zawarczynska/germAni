6d3c31f242c9cd29daa2056ea3fbf2ec46c2cb08
Wojtek Szczawinski <szczawinski.wojtek@gmail.com>
2020-08-16 22:35:34 +0200

    feat(slider): change slider to slick

--- src/app/app.js
+++ src/app/app.js
@@ -1,13 +1,6 @@
-export const run = (stickyNavabar, scrollClasses, favicon, mySwiper) => {
+export const run = (stickyNavabar, scrollClasses, favicon) => {
     window.onscroll = () => {
         stickyNavabar.handleStickyNavbar(), scrollClasses.handleChangeClasses();
     };
     window.addEventListener('DOMContentLoaded', favicon.handleDarkMode());
-
-    let slideInterval = setInterval(slideOpinion, 4000);
-
-    function slideOpinion() {
-        // takes duration of transition as an argument
-        mySwiper.slideNext(1000);
-    }
 };
